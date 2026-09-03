801ec075209b7c0191534702d00b5b8b19bdcaee
FelipeAugustoSan <santos.felipeaugustoger@gmail.com>
2026-09-03 08:03:28 -0300

    feat: estrutura inicial do projeto spring boot

---
+++ src/main/java/com/example/diogoapi/DiogoApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.diogoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiogoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiogoApiApplication.class, args);
+    }
+
+}
